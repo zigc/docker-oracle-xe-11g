f47d5821568b7f4eb3ec484d5f1e53f90a1839ed
Žiga Ciglar <ziga.ciglar@gmail.com>
2016-11-16 19:21:42 +0100

    Initial executes only top install.sql

--- assets/startup.sh
+++ assets/startup.sh
@@ -21,8 +21,7 @@ fi
 
 for f in /docker-entrypoint-initdb.d/*; do
   case "$f" in
-    *.sh)     echo "$0: running $f"; . "$f" ;;
-    *.sql)    echo "$0: running $f"; echo "exit" | /u01/app/oracle/product/11.2.0/xe/bin/sqlplus "SYSTEM/oracle" @"$f"; echo ;;
+    install.sql)    echo "$0: running $f"; echo "exit" | /u01/app/oracle/product/11.2.0/xe/bin/sqlplus "SYSTEM/oracle" @"$f"; echo ;;
     *)        echo "$0: ignoring $f" ;;
   esac
   echo
